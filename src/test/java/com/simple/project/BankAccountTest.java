947529ffa79d8b99a3f2cbd62218b01bb2db969a
Bhoomika1006 <bpanduranga@hawk.iit.edu>
2024-08-31 00:35:21 -0500

    test class added

---
+++ src/test/java/com/simple/project/BankAccountTest.java
@@ -0,0 +1,29 @@
+package com.simple.project;
+import org.junit.Test;
+import static org.junit.Assert.*;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BankAccountTest {
+    @Test
+    public void testDeposit() {
+        BankAccount account = new BankAccount(100);
+        account.deposit(50);
+        assertEquals(150, account.getBalance(), 0.001); // This test will pass
+    }
+
+    @Test
+    public void testWithdraw() {
+        BankAccount account = new BankAccount(100);
+        account.withdraw(50);
+        assertEquals(50, account.getBalance(), 0.001); // This test will pass
+    }
+
+    @Test
+    public void testDepositFail() {
+        BankAccount account = new BankAccount(100);
+        account.deposit(50);
+        assertEquals(200, account.getBalance(), 0.001); // This will fail because the actual balance is 150
+    }
+
+}
